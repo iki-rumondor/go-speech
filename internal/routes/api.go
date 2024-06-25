5c98797df8faa6bcd40e0c7323b530be7d44b4d4
iki-rumondor <ikirumondor07@gmail.com>
2024-06-26 00:07:55 +0800

    feat: allow host baru

--- internal/routes/api.go
+++ internal/routes/api.go
@@ -11,7 +11,7 @@ func StartServer(handlers *config.Handlers) *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://103.26.13.166:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
